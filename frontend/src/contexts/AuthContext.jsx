a28f735e166ec3f3bf108f157e425c862de9d797
Shubham Kumar <shubhamkumar7549yuv@gmail.com>
2026-02-14 01:02:46 +0530

    Removed http-status from frontend

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,62 +1,58 @@
 import axios from "axios";
-import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import server from "../environment";
-
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
     baseURL: `${server}/api/v1/users`
-})
-
+});
 
 export const AuthProvider = ({ children }) => {
 
     const authContext = useContext(AuthContext);
 
-
     const [userData, setUserData] = useState(authContext);
-
 
     const router = useNavigate();
 
     const handleRegister = async (name, username, password) => {
         try {
             let request = await client.post("/register", {
-                name: name,
-                username: username,
-                password: password
-            })
+                name,
+                username,
+                password
+            });
 
-
-            if (request.status === httpStatus.CREATED) {
+            if (request.status === 201) {  // CREATED
                 return request.data.message;
             }
+
         } catch (err) {
             throw err;
         }
-    }
+    };
 
     const handleLogin = async (username, password) => {
         try {
             let request = await client.post("/login", {
-                username: username,
-                password: password
+                username,
+                password
             });
 
-            console.log(username, password)
-            console.log(request.data)
+            console.log(username, password);
+            console.log(request.data);
 
-            if (request.status === httpStatus.OK) {
+            if (request.status === 200) {  // OK
                 localStorage.setItem("token", request.data.token);
-                router("/home")
+                router("/home");
             }
+
         } catch (err) {
             throw err;
         }
-    }
+    };
 
     const getHistoryOfUser = async () => {
         try {
@@ -65,12 +61,13 @@ export const AuthProvider = ({ children }) => {
                     token: localStorage.getItem("token")
                 }
             });
-            return request.data
-        } catch
-         (err) {
+
+            return request.data;
+
+        } catch (err) {
             throw err;
         }
-    }
+    };
 
     const addToUserHistory = async (meetingCode) => {
         try {
@@ -78,21 +75,26 @@ export const AuthProvider = ({ children }) => {
                 token: localStorage.getItem("token"),
                 meeting_code: meetingCode
             });
-            return request
+
+            return request;
+
         } catch (e) {
             throw e;
         }
-    }
-
+    };
 
     const data = {
-        userData, setUserData, addToUserHistory, getHistoryOfUser, handleRegister, handleLogin
-    }
+        userData,
+        setUserData,
+        addToUserHistory,
+        getHistoryOfUser,
+        handleRegister,
+        handleLogin
+    };
 
     return (
         <AuthContext.Provider value={data}>
             {children}
         </AuthContext.Provider>
-    )
-
-}
+    );
+};
